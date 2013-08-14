3ce898a9defbbf94af8172384fec3a65825846bd
Mark Folashade <ayodeji.folashade@gmail.com>
2013-08-14 04:43:32 -0500

    Added raw_input

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,7 +1,13 @@
-cost = 20.00
-tax = 3.00
-tip_rate = .15
-tip = (tip_rate*(cost + tax))
+#cost is cost of the meal
+cost = float(raw_input("The cost of the meal is $"))
+tax = float(raw_input("The Tax is $"))
+tax_value = tax
+meal_with_tax = cost + tax
+tip_rate = float((raw_input("The tip is %")))
+tip_rate /=100
+tip = (tip_rate*(meal_with_tax))
+tip_value = tip
+total = meal_with_tax + tip_value
 print "The base cost of your meal was ${0}".format("%0.2f" % cost)
 print "You need to pay ${0} for tax".format("%0.2f" % tax)
 print "Tipping at a rate of {0}%, you should leave ${1} for a tip.".format("%0.2f" % (tip_rate*100),"%0.2f" %  tip)
